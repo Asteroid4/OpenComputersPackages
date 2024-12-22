fde47fd91f3f240b292fa01a0d9ae479f3bd0d66
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2024-12-22 16:47:58 -0500

    Update brent.lua

--- brent.lua
+++ brent.lua
@@ -3,10 +3,20 @@ local filesystem = require("filesystem")
 local brent = {}
 
 function brent.install(package)
-  if filesystem.exists("/libs/" .. package .. ".lua") then
-    filesystem.remove("/libs/" .. package .. ".lua")
+  if filesystem.exists("/lib/" .. package .. ".lua") then
+    filesystem.remove("/lib/" .. package .. ".lua")
   end
-  shell.execute("wget https://raw.githubusercontent.com/Asteroid/OpenComputersPackages/refs/heads/main/" .. package .. ".lua /libs/" .. package .. ".lua")
+  shell.execute("wget https://raw.githubusercontent.com/Asteroid4/OpenComputersPackages/refs/heads/main/" .. package .. ".lua /lib/" .. package .. ".lua")
+  print("Installed " .. package .. "successfully!")
+end
+
+function brent.uninstall(package)
+  if filesystem.exists("/lib/" .. package .. ".lua") then
+    filesystem.remove("/lib/" .. package .. ".lua")
+    print("Uninstalled " .. package .. " successfully!")
+  else
+    print("That package is not installed!")
+  end
 end
 
 return brent
