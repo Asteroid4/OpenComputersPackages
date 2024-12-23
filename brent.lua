372d7ab8d2496fd31cbe8cbe7daefb4cc8c4d9a8
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2024-12-22 20:33:25 -0500

    Update brent.lua

--- brent.lua
+++ brent.lua
@@ -8,12 +8,34 @@ function brent.install(package)
   end
   shell.execute("wget https://raw.githubusercontent.com/Asteroid4/OpenComputersPackages/refs/heads/main/" .. package .. ".lua /lib/" .. package .. ".lua")
   print("Installed " .. package .. "successfully!")
+  io.write("Reboot? (Y/n) ")
+  if io.read() == "Y" then
+    computer.shutdown(true)
+  end
 end
 
 function brent.uninstall(package)
   if filesystem.exists("/lib/" .. package .. ".lua") then
     filesystem.remove("/lib/" .. package .. ".lua")
     print("Uninstalled " .. package .. " successfully!")
+    io.write("Reboot? (Y/n) ")
+    if io.read() == "Y" then
+      computer.shutdown(true)
+    end
+  else
+    print("That package is not installed!")
+  end
+end
+
+function brent.update(package)
+  if filesystem.exists("/lib/" .. package .. ".lua") then
+    filesystem.remove("/lib/" .. package .. ".lua")
+    shell.execute("wget https://raw.githubusercontent.com/Asteroid4/OpenComputersPackages/refs/heads/main/" .. package .. ".lua /lib/" .. package .. ".lua")
+    print("Updated " .. package .. " successfully!")
+    io.write("Reboot? (Y/n) ")
+    if io.read() == "Y" then
+      computer.shutdown(true)
+    end
   else
     print("That package is not installed!")
   end
