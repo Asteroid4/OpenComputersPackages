2015410778b194c303ba4ede7368ada8e84bc929
Asteroid4 <83736854+Asteroid4@users.noreply.github.com>
2024-12-24 10:23:27 -0500

    Convert brent to command line app, add help page

--- brent.lua
+++ brent.lua
@@ -2,18 +2,10 @@ local shell = require("shell")
 local filesystem = require("filesystem")
 local computer = require("computer")
 local internet = require("internet")
-local brent = {}
+local args, options = shell.parse(...)
 
-function brent.list()
-  result = ""
-  for chunk in internet.request("https://raw.githubusercontent.com/Asteroid4/OpenComputersPackages/refs/heads/main/list")
-  do result = result..chunk end
-  print(result)
-end
-
-function brent.install(package, noprompt)
+function install(package, noprompt)
   if filesystem.exists("/lib/" .. package .. ".lua") then
-    filesystem.remove("/lib/" .. package .. ".lua")
     print("That package is already installed!")
   else
     shell.execute("wget https://raw.githubusercontent.com/Asteroid4/OpenComputersPackages/refs/heads/main/" .. package .. ".lua /lib/" .. package .. ".lua")
@@ -27,7 +19,7 @@ function brent.install(package, noprompt)
   end
 end
 
-function brent.uninstall(package, noprompt)
+function uninstall(package, noprompt)
   if filesystem.exists("/lib/" .. package .. ".lua") then
     filesystem.remove("/lib/" .. package .. ".lua")
     print("Uninstalled " .. package .. " successfully!")
@@ -42,9 +34,28 @@ function brent.uninstall(package, noprompt)
   end
 end
 
-function brent.update(package, noprompt)
-  brent.uninstall(package, true)
-  brent.install(package, noprompt)
+function help()
+  print([[Usage:
+  brent list
+  brent install <package>
+  brent uninstall <package>
+  brent update <package>]])
 end
 
-return brent
+if options.h then
+  help()
+elseif args[0] == "list" then
+  result = ""
+  for chunk in internet.request("https://raw.githubusercontent.com/Asteroid4/OpenComputersPackages/refs/heads/main/list")
+  do result = result..chunk end
+  print(result)
+elseif args[0] == "install" then
+  install(false)
+elseif args[0] == "uninstall" then
+  uninstall(false)
+elseif args[0] == "update" then
+  brent.uninstall(package, true)
+  brent.install(package, false)
+else
+  help()
+end
