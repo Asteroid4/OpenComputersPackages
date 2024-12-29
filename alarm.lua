a3b2aaddf00f41cc1da596776398376790b08e5c
Asteroid4 <83736854+Asteroid4@users.noreply.github.com>
2024-12-29 13:13:43 -0500

    fix up alarm, add base

--- alarm.lua
+++ alarm.lua
@@ -4,24 +4,16 @@ local sides = require("sides")
 local os = require("os")
 local alarm = {}
 
-function activate()
-    component.redstone.setOutput(sides.top, 15)
-end
-
-function deactivate()
-    component.redstone.setOutput(sides.top, 0)
-end
-
 function alarm.arm()
     component.modem.open(5042)
     while (true) do
-        local _, _, _, _, _, on = event.pull("modem_message")
+        local _, _, from, _, _, on = event.pull("modem_message")
         if (on) then
             component.redstone.setOutput(sides.top, 15)
-            print(os.time() .. ": Alarm activated!")
+            print(os.time() .. ": Alarm activated by " .. from .. "!")
         else
             component.redstone.setOutput(sides.top, 0)
-            print(os.time() .. ": Alarm deactivated!")
+            print(os.time() .. ": Alarm deactivated by " .. from .. "!")
         end
     end
 end
