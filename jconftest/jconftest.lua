5a60a84dfb9b49eef4d321371db1980350bca502
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2025-07-13 17:33:51 -0400

    Update jconftest.lua

--- jconftest/jconftest.lua
+++ jconftest/jconftest.lua
@@ -1,5 +1,5 @@
-config-path = "/usr/etc/jconftest.cfg"
-default-config-path = "/usr/etc/jconftest.cfg.d"
+config-path = "/etc/jconftest.cfg"
+default-config-path = "/etc/jconftest.cfg.d"
 io.write("Searching for config file...")
 if filesystem.exists(config-path) then
   io.write("Config exists, test complete!")
