f1ab06ebddf4b6e1910fdd8dfac735930b03aee8
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2025-07-13 17:24:25 -0400

    Create jconftest.lua

---
+++ jconftest/jconftest.lua
@@ -0,0 +1,18 @@
+config-path = "/etc/jconftest.cfg"
+default-config-path = "/etc/jconftest.cfg.d"
+io.write("Searching for config file...")
+if filesystem.exists(config-path) then
+  io.write("Config exists, test complete!")
+else
+  io.write("No config exists, searching for default...")
+  if filesystem.exists(default-config-path) then
+    io.write("Default found!")
+    if filesystem.copy(default-config-path,  config-path) then
+      io.write("Copied default to config. Test complete!")
+    else
+      io.write("Copy failed, test failed.")
+    end
+  else
+      io.write("No default found! Test failed.")
+  end
+end
