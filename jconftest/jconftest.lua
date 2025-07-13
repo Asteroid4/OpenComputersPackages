f37cd47d87833c2063fdc19a411538b8fecd9fdb
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2025-07-13 18:02:29 -0400

    Update jconftest.lua

--- jconftest/jconftest.lua
+++ jconftest/jconftest.lua
@@ -1,13 +1,15 @@
-config_path = "/etc/jconftest.cfg"
-default_config_path = "/etc/jconftest.cfg.d"
+local fs = require("filesystem")
+local config_path = "/etc/jconftest.cfg"
+local default_config_path = "/etc/jconftest.cfg.d"
+
 io.write("Searching for config file...\n")
-if filesystem.exists(config_path) then
+if fs.exists(config_path) then
   io.write("Config exists, test complete!\n")
 else
   io.write("No config exists, searching for default...\n")
-  if filesystem.exists(default_config_path) then
+  if fs.exists(default_config_path) then
     io.write("Default found!\n")
-    if filesystem.copy(default_config_path,  config_path) then
+    if fs.copy(default_config_path,  config_path) then
       io.write("Copied default to config. Test complete!\n")
     else
       io.write("Copy failed, test failed.\n")
