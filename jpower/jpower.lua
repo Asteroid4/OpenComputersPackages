a339b20287a32ed83b637105b8c3229fc10f01e9
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2025-07-14 15:39:10 -0400

    Create jpower.lua

---
+++ jpower/jpower.lua
@@ -0,0 +1,38 @@
+local fs = require("filesystem")
+local serial = require("serialization")
+local redstone
+
+local config_path = "/etc/jpower.cfg"
+local default_config_path = "/etc/jpower.cfg.d"
+
+if component
+
+function main(config)
+  io.write(string.format("foo is %d and bar is %d", config.foo, config.bar))
+end
+
+function load_config()
+  local file = io.open(config_path)
+  local config = serial.unserialize(file:read("*a"))
+  io.write("Config loaded successfully!\n")
+  main(config)
+end
+
+io.write("Searching for config file...\n")
+if fs.exists(config_path) then
+  io.write("Config found. Loading config... ")
+  load_config()
+else
+  io.write("No config exists, searching for default... ")
+  if fs.exists(default_config_path) then
+    io.write("Default config found.\n")
+    if fs.copy(default_config_path,  config_path) then
+      io.write("Created config using default. Loading config... ")
+      load_config()
+    else
+      io.write("Copy failed. Exiting...\n")
+    end
+  else
+      io.write("\nNo default config found. Exiting...\n")
+  end
+end
