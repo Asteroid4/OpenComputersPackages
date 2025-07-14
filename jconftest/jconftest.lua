9cb4d62ce46f1e1c3df864e8080316c7b6a9061f
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2025-07-14 07:56:51 -0400

    Update jconftest.lua

--- jconftest/jconftest.lua
+++ jconftest/jconftest.lua
@@ -1,20 +1,36 @@
 local fs = require("filesystem")
+local serial = require("serialization")
+
+local config = {}
 local config_path = "/etc/jconftest.cfg"
 local default_config_path = "/etc/jconftest.cfg.d"
 
+function main()
+  io.write(string.format("foo is %d and bar is %d", config.foo, config.bar))
+end
+
+function load_config()
+  file = io.open(config_path)
+  config = serial.unserialize(file.read("*a"))
+  io.write("Config loaded successfully!\n")
+  main()
+end
+
 io.write("Searching for config file...\n")
 if fs.exists(config_path) then
-  io.write("Config exists, test complete!\n")
+  io.write("Config found. Loading config...\n")
+  load_config()
 else
   io.write("No config exists, searching for default...\n")
   if fs.exists(default_config_path) then
-    io.write("Default found!\n")
+    io.write("Default config found.\n")
     if fs.copy(default_config_path,  config_path) then
-      io.write("Copied default to config. Test complete!\n")
+      io.write("Created config using default. Loading config...\n")
+      load_config()
     else
-      io.write("Copy failed, test failed.\n")
+      io.write("Copy failed. Exiting...\n")
     end
   else
-      io.write("No default found! Test failed.\n")
+      io.write("No default config found. Exiting...\n")
   end
 end
