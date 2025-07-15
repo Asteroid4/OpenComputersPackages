6efb3838bc1d170721c15f2bf8b521f088a0e872
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2025-07-15 13:57:18 -0400

    Update jpower.cfg.d

--- jpower/jpower.cfg.d
+++ jpower/jpower.cfg.d
@@ -3,11 +3,14 @@
     power_remaining_side = 4,
     bundled_output_side = 2,
     bundled_input_side = 3,
+    alarm_output_side = 0,
 
     generator_color = 14,
-    alarm_color = 15,
 
     generator_activate_threshold = 4,
     generator_deactivate_threshold = 14,
     alarm_threshold = 2,
+
+    low_machines = {0, 1, 2},
+    high_machines = {3, 4, 5}
 }
