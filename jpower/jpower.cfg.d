5d3596404d781ce8618d8bc60ca95ea65864169b
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2025-07-15 08:24:14 -0400

    Update jpower.cfg.d

--- jpower/jpower.cfg.d
+++ jpower/jpower.cfg.d
@@ -1,6 +1,6 @@
 {
-    manual_override_side = 1,
-    power_remaining_side = 0,
+    manual_override_side = 5,
+    power_remaining_side = 4,
     bundled_output_side = 2,
     bundled_input_side = 3,
 
