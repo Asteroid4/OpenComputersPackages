62ffb2eb97c6948358c6041daefb01687eef7f71
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2026-01-29 18:40:31 -0500

    Update purificationtier8.lua

--- purificationtier8/purificationtier8.lua
+++ purificationtier8/purificationtier8.lua
@@ -1,7 +1,7 @@
 local fs = require("filesystem")
 local serial = require("serialization")
 local component = require("component")
-local redstone = component.redstone
+local redstone
 local transposer_quark_in
 local transposer_quark_out
 local transposer_fluid_out
@@ -12,13 +12,29 @@ local default_config_path = "/etc/purificationtier8.cfg.d"
 
 function main(config)
   if version > config.version then
-    io.write(string.format("The program is using version %d, which is newer than the config file's version, %d.\n", version, config.version))
+    io.write(string.format("[WARN] The program is using version %d, which is newer than the config file's version, %d.\n", version, config.version))
   elseif version < config.version then
-    io.write(string.format("The program is using version %d, which is older than the config file's version, %d.\n", version, config.version))
+    io.write(string.format("[WARN] The program is using version %d, which is older than the config file's version, %d.\n", version, config.version))
   end
   transposer_quark_in = component.proxy(component.get(config.transposer_quark_in_addr))
   transposer_quark_out = component.proxy(component.get(config.transposer_quark_out_addr))
   transposer_fluid_out = component.proxy(component.get(config.transposer_fluid_out_addr))
+  error = false
+  if transposer_quark_in == nil then
+    io.stderr:write(string.format("[ERROR] Unable to find quark input transposer by address of %s.", config.transposer_quark_in_addr))
+    error = true
+  end
+  if transposer_quark_out == nil then
+    io.stderr:write(string.format("[ERROR] Unable to find quark output transposer by address of %s.", config.transposer_quark_out_addr))
+    error = true
+  end
+  if transposer_fluid_out == nil then
+    io.stderr:write(string.format("[ERROR] Unable to find fluid output transposer by address of %s.", config.transposer_fluid_out_addr))
+    error = true
+  end
+  if error then
+    os.exit()
+  end
   local quark_sequence = {1,2,3,4,5,6, 1,3,5,2,6,4, 1,5,2,4,3,6}
   local quark_index = 1
   local sane = true
@@ -30,7 +46,7 @@ function main(config)
     end
     os.sleep(0.05)
   end
-  io.write("[ERROR] Unknown error detected! Shutting down...")
+  io.stderr:write("[ERROR] Unknown error detected! Shutting down...")
 end
 
 function load_config()
@@ -40,7 +56,7 @@ function load_config()
   main(config)
 end
 
-io.write("Searching for config file...\n")
+io.write("[INFO] Searching for config file...\n")
 if fs.exists(config_path) then
   io.write("Config found. Loading config... ")
   load_config()
@@ -52,9 +68,9 @@ else
       io.write("Created config using default. Loading config... ")
       load_config()
     else
-      io.write("Copy failed. Exiting...\n")
+      io.write("[ERROR] Copy failed. Exiting...\n")
     end
   else
-      io.write("\nNo default config found. Exiting...\n")
+      io.write("\n[INFO] No default config found. Exiting...\n")
   end
 end
