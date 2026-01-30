0c2781d9cfd53996d949fe9a98c3776dfa9619bc
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2026-01-29 20:30:44 -0500

    Update purificationtier8.cfg.d

--- purificationtier8/purificationtier8.cfg.d
+++ purificationtier8/purificationtier8.cfg.d
@@ -3,5 +3,14 @@
 
   transposer_quark_in_addr = "",
   transposer_quark_out_addr = "",
-  transposer_fluid_out_addr = ""
+  transposer_fluid_out_addr = "",
+
+  up_quark_realignment_side_on_quark_in = 1,
+  down_quark_realignment_side_on_quark_in = 3,
+  bottom_quark_realignment_side_on_quark_in = 5,
+  top_quark_realignment_side_on_quark_out = 1,
+  strange_quark_realignment_side_on_quark_out = 2,
+  charm_quark_realignment_side_on_quark_out = 4,
+
+  fluid_output_side = 1
 }
