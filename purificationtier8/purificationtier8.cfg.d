73cd2715d5f7416b3508462d77b07cbabb92f851
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2026-02-01 22:35:38 -0500

    Update purificationtier8.cfg.d

--- purificationtier8/purificationtier8.cfg.d
+++ purificationtier8/purificationtier8.cfg.d
@@ -1,11 +1,10 @@
 {
-  version = 1,
+  version = 2,
 
   transposer_quark_in_addr = "",
   transposer_quark_out_addr = "",
   transposer_fluid_out_addr = "",
 
-  clock_restart_signal_computer_side = 5,
   machine_active_signal_computer_side = 4,
 
   up_quark_realignment_side_on_quark_in = 1,
