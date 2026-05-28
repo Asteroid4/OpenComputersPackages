ef8acb331dd4cec859f311db855ce2e5da829f05
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2026-05-28 09:42:31 -0400

    Update base-manager.cfg.d

--- base-manager/base-manager.cfg.d
+++ base-manager/base-manager.cfg.d
@@ -2,6 +2,8 @@
   version = 1,
 
   monitored_components = {
-    component_address_here = "name_of_component."
+    unimportant_component_address = "Name of this component.",
+    another_unimportant_component = "It tells you if the component cant be monitored right at startup.",
+    this_one_is_critical = "Notice the punctuation as that is the important part!"
   }
 }
