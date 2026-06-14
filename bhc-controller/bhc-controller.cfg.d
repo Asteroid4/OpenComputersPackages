887f2fe208f3636d34c0cbe2921cdf7ce22fcd05
Asteroid <83736854+Asteroid4@users.noreply.github.com>
2026-06-14 12:06:35 -0400

    Update bhc-controller.cfg.d

--- bhc-controller/bhc-controller.cfg.d
+++ bhc-controller/bhc-controller.cfg.d
@@ -1,12 +1,13 @@
 {
-  version = 1,
+  version = 2,
 
   black_hole_active_side_input = 2,
   recipes_ready_side_input = 3,
-  recipes_inputs_side_output = 4,
   spacetime_inputs_side_output = 5,
 
   input_side_on_transposer = 0,
   black_hole_seed_side_on_transposer = 3,
-  black_hole_collapser_side_on_transposer = 4
+  black_hole_collapser_side_on_transposer = 4,
+
+  min_spacetime_time = 0
 }
